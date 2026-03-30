75c08528f8be58ae9bbed8f591e35d5ed1186c3f
patricio1000 <patricio.estradat@gmail.com>
2026-03-30 00:18:11 -0500

    Completed friendFavorites filtering and longest food challenge I wrote all the code and I forgot to commit it, so here it is all in one commit. I also added a console log to show the longest food name and its length.

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ for (let i = 0; i < friendFavorites.length; i++) {
 console.log("Foods with 'a':", foodsWithA);
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
+
 let longFoodNames = [];
 
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
